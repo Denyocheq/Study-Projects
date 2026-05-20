8c37b8d8a5bc2babe25f293b93dd7fe7041d0915
Denyocheq <denyocheq@gmail.com>
2026-05-20 23:08:46 +0300

    Added a couple of JS scripts and fixed a bug in the portfolio.
    Added several scripts for completing training tasks.
    
    A bug was discovered in the portfolio: on phones, 100vh enabled the browser's address bar, causing blocks to appear higher on the screen and the scroll button to move down. I decided to use dvh with the fallback vh.

---
+++ freeCodeCamp/JavaScript Certification/(01) - Trivia Bot/script.js
@@ -0,0 +1,33 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Yasuo";
+let botLocation = "Tokyo";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact =
+  "Fun fact: When " +
+  favoriteLanguage +
+  " was first created, it had a different name – 'LiveScript.' However, Java was very popular at the time, and it was decided that positioning JavaScript as Java's 'little brother' would be beneficial.";
+
+console.log(codingFact);
+
+codingFact =
+  "Another fun fact: " +
+  favoriteLanguage +
+  " was invented in just 10 days. In 1995, programmer Brendan Eich was working at Netscape and was given just 10 days to create a browser language. He succeeded. The first version was a bit rough around the edges, but the language took off. And to this day, many of " +
+  favoriteLanguage +
+  "'s quirks stem precisely from that rush.";
+
+console.log(codingFact);
+
+codingFact =
+  "Third fun fact: For the first two weeks after " +
+  favoriteLanguage +
+  "'s creation, no one knew about it except Brendan Eich himself. He sat and wrote code alone in his room. No discussions with other developers, no emails on mailing lists—just 10 days of intense work, followed by another two weeks of silence.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
---
+++ freeCodeCamp/JavaScript Certification/(02) - Sentence Maker/script.js
@@ -0,0 +1,51 @@
+let adjective = "beauty";
+let adjective2 = "cute";
+let noun = "cat";
+let verb = "angry";
+let place = "home";
+let noun2 = "fish";
+
+let firstStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "old";
+adjective2 = "smoking";
+noun = "dragon";
+verb = "sleep";
+place = "cave";
+noun2 = "people";
+
+let secondStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+console.log("Second story: " + secondStory);
